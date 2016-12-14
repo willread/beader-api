050755f7b33086b17e846196492bccb875596b1f
William Blanchette <wb@wblanchette.com>
2016-12-14 15:18:50 -0700

    Merged headers

--- api.js
+++ api.js
@@ -20,10 +20,7 @@ app.use(bodyParser.json({limit: '5mb'}));
 
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:9000');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-    res.header('Access-Control-Allow-Headers', 'content-type');
-  res.header('Access-Control-Allow-Headers', 'Authorization');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
