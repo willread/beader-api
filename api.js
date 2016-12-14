e5bb6f19917897e795514c2788c20574a4a416ed
William Blanchette <wb@wblanchette.com>
2016-12-14 13:55:54 -0700

    Added bracket

--- api.js
+++ api.js
@@ -86,54 +86,55 @@ app.post('/patterns', function(req, res) {
     };
 
     request.post(accessTokenUrl, { json: true, form: params }, function(err, response, token) {
-    var accessToken = token.access_token;
-    var headers = { Authorization: 'Bearer ' + accessToken };
+      var accessToken = token.access_token;
+      var headers = { Authorization: 'Bearer ' + accessToken };
 
-    // Step 2. Retrieve profile information about the current user.
-    request.get({ url: peopleApiUrl, headers: headers, json: true }, function(err, response, profile) {
+      // Step 2. Retrieve profile information about the current user.
+      request.get({ url: peopleApiUrl, headers: headers, json: true }, function(err, response, profile) {
 
-      // Step 3a. Link user accounts.
-      if (req.headers.authorization) {
-        User.findOne({ google: profile.sub }, function(err, existingUser) {
-          if (existingUser) {
-            return res.status(409).send({ message: 'There is already a Google account that belongs to you' });
-          }
-          var token = req.headers.authorization.split(' ')[1];
-          var payload = jwt.decode(token, config.TOKEN_SECRET);
-          User.findById(payload.sub, function(err, user) {
-            if (!user) {
-              return res.status(400).send({ message: 'User not found' });
+        // Step 3a. Link user accounts.
+        if (req.headers.authorization) {
+          User.findOne({ google: profile.sub }, function(err, existingUser) {
+            if (existingUser) {
+              return res.status(409).send({ message: 'There is already a Google account that belongs to you' });
             }
+            var token = req.headers.authorization.split(' ')[1];
+            var payload = jwt.decode(token, config.TOKEN_SECRET);
+            User.findById(payload.sub, function(err, user) {
+              if (!user) {
+                return res.status(400).send({ message: 'User not found' });
+              }
+              user.google = profile.sub;
+              user.picture = user.picture || profile.picture.replace('sz=50', 'sz=200');
+              user.displayName = user.displayName || profile.name;
+              user.save(function() {
+                var token = authUtils.createJWT(user);
+                console.log("token sent");
+                res.send({ token: token });
+              });
+            });
+          });
+        } else {
+          // Step 3b. Create a new user account or return an existing one.
+          User.findOne({ google: profile.sub }, function(err, existingUser) {
+            if (existingUser) {
+                console.log("token sent");
+
+              return res.send({ token: authUtils.createJWT(existingUser) });
+            }
+            var user = new User();
             user.google = profile.sub;
-            user.picture = user.picture || profile.picture.replace('sz=50', 'sz=200');
-            user.displayName = user.displayName || profile.name;
-            user.save(function() {
+            user.picture = profile.picture.replace('sz=50', 'sz=200');
+            user.displayName = profile.name;
+            user.save(function(err) {
               var token = authUtils.createJWT(user);
-              console.log("token sent");
+                console.log("token sent");
+
               res.send({ token: token });
             });
           });
-        });
-      } else {
-        // Step 3b. Create a new user account or return an existing one.
-        User.findOne({ google: profile.sub }, function(err, existingUser) {
-          if (existingUser) {
-              console.log("token sent");
-
-            return res.send({ token: authUtils.createJWT(existingUser) });
-          }
-          var user = new User();
-          user.google = profile.sub;
-          user.picture = profile.picture.replace('sz=50', 'sz=200');
-          user.displayName = profile.name;
-          user.save(function(err) {
-            var token = authUtils.createJWT(user);
-              console.log("token sent");
-
-            res.send({ token: token });
-          });
-        });
-      }
+        }
+      });
     });
   });
 });
