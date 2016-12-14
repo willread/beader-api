aa683aa9ad507eeaadda472bf838b1d103a46949
William Blanchette <wb@wblanchette.com>
2016-12-14 14:00:04 -0700

    Added header

--- api.js
+++ api.js
@@ -19,6 +19,7 @@ app.all('*', function(req, res, next) {
    res.header('Access-Control-Allow-Origin', 'http://localhost:9000');
    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
    res.header('Access-Control-Allow-Headers', 'Content-Type');
+   res.header('Access-Control-Allow-Credentials', 'true');
    next();
 });
 
