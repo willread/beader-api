74d7b864652742ece0a678f0ed567e8647fda3b0
William Blanchette <wb@wblanchette.com>
2016-12-14 13:58:54 -0700

    Changed origin

--- api.js
+++ api.js
@@ -16,7 +16,7 @@ var app = express();
 app.use(bodyParser.json({limit: '5mb'}));
 
 app.all('*', function(req, res, next) {
-   res.header('Access-Control-Allow-Origin', '*');
+   res.header('Access-Control-Allow-Origin', 'http://localhost:9000');
    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
    res.header('Access-Control-Allow-Headers', 'Content-Type');
    next();
