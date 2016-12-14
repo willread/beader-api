208da319e2c509bbd722cacdce9636274d6a00de
William Blanchette <wb@wblanchette.com>
2016-12-14 15:12:22 -0700

    jwt now contains user

--- authUtils.js
+++ authUtils.js
@@ -4,7 +4,7 @@ var jwt = require('jwt-simple');
 
 exports.createJWT = function(user){
     var payload = {
-        sub: user._id,
+        user: user,
         iat: moment().unix(),
         exp: moment().add(14, 'days').unix()
     };
