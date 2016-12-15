8269b06999d1f3bfa59c0da5da16fb43e5a358b6
William Blanchette <will@Williams-MBP.hitronhub.home>
2016-12-15 08:57:28 -0700

    Fixed typo

--- patterns.js
+++ patterns.js
@@ -66,7 +66,7 @@ router.get('/:id', function(req, res) {
     if(err){
       return res.status(404).json({message: 'Pattern not found.'});
     }
-    console.log('pattern:', pattern, 'id:' req.params.id);
+    console.log('pattern:', pattern, 'id:', req.params.id);
     res.json(pattern);
   });
 });
