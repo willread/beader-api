3c718e1a872d0c5b5ad448b1182cac386c18ef78
William Blanchette <wb@wblanchette.com>
2016-12-14 19:17:01 -0700

    Added check for OPTIONS calls

--- authUtils.js
+++ authUtils.js
@@ -31,22 +31,25 @@ exports.handleError = function (res, err) {
 }
 
 exports.ensureAuthenticated = function(req, res, next) {
-  if (!req.headers.authorization) {
-    return res.status(401).send({ message: 'Please make sure your request has an Authorization header' });
-  }
-  var token = req.headers.authorization.split(' ')[1];
+  if(req.method != 'OPTIONS'){
+    if (!req.headers.authorization) {
+      return res.status(401).send({ message: 'Please make sure your request has an Authorization header' });
+    }
+    var token = req.headers.authorization.split(' ')[1];
 
-  var payload = null;
-  try {
-    payload = jwt.decode(token, process.env.TOKEN_SECRET);
-  }
-  catch (err) {
-    return res.status(401).send({ message: err.message });
+    var payload = null;
+    try {
+      payload = jwt.decode(token, process.env.TOKEN_SECRET);
+    }
+    catch (err) {
+      return res.status(401).send({ message: err.message });
+    }
+
+    if (payload.exp <= moment().unix()) {
+      return res.status(401).send({ message: 'Token has expired' });
+    }
+    req.user = payload.user;
   }
 
-  if (payload.exp <= moment().unix()) {
-    return res.status(401).send({ message: 'Token has expired' });
-  }
-  req.user = payload.user;
   next();
 }
