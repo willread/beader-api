0da58bee26a997927df42411e8e8f77336b26ed6
William Blanchette <will@Williams-MBP.hitronhub.home>
2016-12-15 09:11:26 -0700

    Some routes don't need to be authenticated

--- patterns.js
+++ patterns.js
@@ -7,7 +7,6 @@ var db = require('./db').getDb;
 // Configure router
 
 var router = express.Router();
-router.use(authUtils.ensureAuthenticated);
 
 // Configure image upload SDK
 
@@ -16,6 +15,34 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
+// Get a list of patterns
+
+// GET /patterns
+
+router.get('/', function(req, res) {
+  db().collection('patterns').find().limit(5).skip(0).toArray(function(err, patterns) {
+      res.json(patterns);
+  });
+});
+
+// Get a single pattern
+
+// GET /pattern/:id
+
+router.get('/:id', function(req, res) {
+  db().collection('patterns').findOne({_id: new mongo.ObjectId(req.params.id)}, function(err, pattern) {
+    if(err){
+      return res.status(404).json({message: 'Pattern not found.'});
+    }
+    console.log('pattern:', pattern, 'id:', req.params.id);
+    res.json(pattern);
+  });
+});
+
+// Routes after this are authenticated
+
+router.use(authUtils.ensureAuthenticated);
 
 // Create a new pattern
 
@@ -46,30 +73,6 @@ router.post('/', function(req, res) {
       }
     })
   }, {});
-});
-
-// Get a list of patterns
-
-// GET /patterns
-
-router.get('/', function(req, res) {
-  db().collection('patterns').find().limit(5).skip(0).toArray(function(err, patterns) {
-      res.json(patterns);
-  });
-});
-
-// Get a single pattern
-
-// GET /pattern/:id
-
-router.get('/:id', function(req, res) {
-  db().collection('patterns').findOne({_id: new mongo.ObjectId(req.params.id)}, function(err, pattern) {
-    if(err){
-      return res.status(404).json({message: 'Pattern not found.'});
-    }
-    console.log('pattern:', pattern, 'id:', req.params.id);
-    res.json(pattern);
-  });
 });
 
 module.exports = router;
