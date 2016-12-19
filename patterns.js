1e50b7f3fcf4290016a9f4a69c81c413ab52d99e
William Blanchette <will@Williams-MBP.hitronhub.home>
2016-12-19 14:19:21 -0700

    Removed _

--- patterns.js
+++ patterns.js
@@ -124,8 +124,7 @@ router.delete('/:id', function(req, res) {
         return res.status(404).json({message: 'Pattern not found.', error: err.message});
       }
 
-      if(pattern.user._id !== req.user._id){
-        console.log(pattern.user.id, req.user._id);
+      if(pattern.user.id !== req.user._id){
         return res.status(403).json({message: 'You are not alloed to delete this pattern'});
       }
 
