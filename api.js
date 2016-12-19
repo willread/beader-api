49213fa27667576b9f21a4c4556f156163ce0b61
William Blanchette <will@Williams-MBP.hitronhub.home>
2016-12-19 13:22:57 -0700

    Tweaked allowed origin

--- api.js
+++ api.js
@@ -19,7 +19,7 @@ app.use(expressValidator());
 // Allow cross-origin
 
 app.all('*', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://collectivecognition.github.io/beader.org'); // FIXME
+  res.header('Access-Control-Allow-Origin', 'http://collectivecognition.github.io'); // FIXME
   res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
   next();
