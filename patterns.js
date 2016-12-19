df69ba9641d7725720a775750f2622a7573ae4cf
William Blanchette <will@Williams-MBP.hitronhub.home>
2016-12-18 20:39:17 -0700

    Added patterns by user endpoint

--- patterns.js
+++ patterns.js
@@ -40,6 +40,20 @@ router.get('/', function(req, res) {
   });
 });
 
+// Get all patterns by a user
+
+router.use(paginate.middleware(10, 50));
+
+router.get('/user/:id', function(req, res) {
+  Pattern.paginate({user: req.params.id}, {page: req.query.page, limit: req.query.limit, populate: 'user'}, function(err, result){
+    res.json({
+      patterns: result.docs,
+      totalPages: result.pages,
+      totalPatterns: result.total
+    });
+  });
+});
+
 // Get a single pattern
 
 // GET /pattern/:id
